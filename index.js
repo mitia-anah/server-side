b0958234254f60d39f7f7ba6bcdeffa7b80779e6
Rosny Ratsianompo <rosny.rat@onja.org>
2020-08-25 11:43:03 +0100

    we installed nunjucks and nodemon to power html

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const nunjucks = require('nunjucks');
+
 console.log('Greetings from Node and index.js! 🙃');
 
 // We need Express to act as our application server
@@ -5,15 +7,17 @@ const express = require('express');
 
 // Let's create a new App
 const app = express();
+nunjucks.configure({
+    autoescape: true,
+    express: app
+});
 
 // We want express to run on this port
 const port = 3000;
 
 app.get('/', (request, response) => {
-    console.log('Request: /');
-    response.sendFile(__dirname + '/index.html');
+    response.render('./views/index.html.njk');
 });
-
 app.listen(port, () => {
     console.log(`app is running here: http://localhost:${port}`);
 });
